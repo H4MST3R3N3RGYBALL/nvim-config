de1ba8503d57f6eb56326ad552891946106b13bd
h4mst3r <53465855+H4MST3R3N3RGYBALL@users.noreply.github.com>
2024-11-16 18:50:36 -0500

    CMP IS PAIN

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazyvim_python_lsp = "basedpyright"
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "*",
+    callback = function(opts)
+        if vim.bo[opts.buf].omnifunc == "" then
+            vim.bo[opts.buf].omnifunc = "syntaxcomplete#Complete"
+        end
+    end,
+})
