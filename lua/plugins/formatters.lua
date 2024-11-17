74ef59a3ae2a81a6018c3ccc404c622e61277a3b
h4mst3r <53465855+H4MST3R3N3RGYBALL@users.noreply.github.com>
2024-11-16 22:09:10 -0500

    More fotters

--- lua/plugins/formatters.lua
+++ lua/plugins/formatters.lua
@@ -10,23 +10,16 @@ return {
 
         },
 
-        -- Format options
-        format = {
-            timeout_ms = 3000,
-            async = false, -- Not recommended to change
-            quiet = false, -- Not recommended to change
-        },
-
         -- Formatter by filetype configuration
         formatters_by_ft = {
             lua = { "stylua" },
-            python = { "ruff_organize_imports", "ruff_format" },
+            python = { "ruff_organize_imports", "ruff_format", "ruff_fix" },
             rust = { "rustfmt" },
             swift = { "swiftformat" },
             go = { "goimports", "gofmt" },
-            json = { "clang-format" },
-            yaml = { "yamlfmt" },
-            markdown = { "markdownfmt" },
+            json = { "prettier" },
+            yaml = { "prettier" },
+            markdown = { "prettier" },
             sh = { "shfmt" },
             c = { "clang-format" },
             cpp = { "clang-format" },
@@ -34,11 +27,25 @@ return {
             objcpp = { "clang-format" },
             cuda = { "clang-format" },
             proto = { "clang-format" },
-            m = { "clang-format" },
-            mm = { "clang-format" },
             cs = { "clang-format" },
             verilog = { "verible" },
             systemverilog = { "verible" },
+            cmake = { "gersemi" },
+            sql = { "sqlfmt" },
+            zig = { "zigfmt" },
+            javascript = { "prettier" },
+            typescript = { "prettier" },
+            javascriptreact = { "prettier" },
+            typescriptreact = { "prettier" },
+            vue = { "prettier" },
+            css = { "prettier" },
+            scss = { "prettier" },
+            less = { "prettier" },
+            html = { "prettier" },
+            jsonc = { "prettier" },
+            graphql = { "prettier" },
+            handlebars = { "prettier" },
+            ["*"] = { "trim_newlines", "trim_whitespace" },
         },
         default_format_opts = {
             lsp_format = "fallback",
