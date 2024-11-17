3c30e43fb2d8f5abd788ca6bc238f427783bf9ae
h4mst3r <53465855+H4MST3R3N3RGYBALL@users.noreply.github.com>
2024-11-16 21:23:47 -0500

    Fix nix

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -7,7 +7,7 @@ return {
       require("mason").setup({
         -- Optional: Customize Mason UI or other settings
         ui = {
-          border = "rounded",  -- Can use 'none', 'single', 'double', 'rounded', 'solid'
+          border = "rounded", -- Can use 'none', 'single', 'double', 'rounded', 'solid'
         },
       })
 
@@ -15,31 +15,31 @@ return {
       require("mason-lspconfig").setup({
         -- List of LSP servers to ensure are installed via Mason
         ensure_installed = {
-          "basedpyright",        -- Python (optional)
-          "rust_analyzer",  -- Rust (optional)
-            "arduino_language_server", -- Arduino (optional)
-            "asm_lsp", -- Assembly (optional)
-            "bashls", -- Bash (optional)
-            "ast_grep", -- Code search (optional)
-            "clangd", -- C/C++ (optional)
-            "cmake", -- CMake (optional)
-            "dockerls", -- Docker (optional)
-            "gopls", -- Go (optional)
-            "java_language_server", -- Java (optional)
-            "jsonls", -- JSON (optional)
-            "ltex", -- LaTeX (optional)
-            "lua_ls", -- Lua (optional)
-            "matlab_ls", -- MATLAB (optional)
-            "powershell_es", -- PowerShell (optional)
-            "bufls", -- Protobuf (optional)
-            "hdl_checker", -- SystemVerilog (optional)
-            "lemminx", -- XML (optional)
-            "yamlls", -- YAML (optional)
-            "harper_ls"
+          "basedpyright",            -- Python (optional)
+          "rust_analyzer",           -- Rust (optional)
+          "arduino_language_server", -- Arduino (optional)
+          "asm_lsp",                 -- Assembly (optional)
+          "bashls",                  -- Bash (optional)
+          "ast_grep",                -- Code search (optional)
+          "clangd",                  -- C/C++ (optional)
+          "cmake",                   -- CMake (optional)
+          "dockerls",                -- Docker (optional)
+          "gopls",                   -- Go (optional)
+          "java_language_server",    -- Java (optional)
+          "jsonls",                  -- JSON (optional)
+          "ltex",                    -- LaTeX (optional)
+          "lua_ls",                  -- Lua (optional)
+          "matlab_ls",               -- MATLAB (optional)
+          "powershell_es",           -- PowerShell (optional)
+          "bufls",                   -- Protobuf (optional)
+          "hdl_checker",             -- SystemVerilog (optional)
+          "lemminx",                 -- XML (optional)
+          "yamlls",                  -- YAML (optional)
+          "harper_ls"
 
         },
         -- Optional: Enable automatic installation of LSP servers when they're needed
-        automatic_installation = true,
+        automatic_installation = false,
       })
     end,
   },
