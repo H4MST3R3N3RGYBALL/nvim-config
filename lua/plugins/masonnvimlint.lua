ecc22959fac3394470940fc21ef85df82f257d48
h4mst3r <53465855+H4MST3R3N3RGYBALL@users.noreply.github.com>
2024-11-17 00:09:37 -0500

    Fix keybinds for git cmds

--- lua/plugins/masonnvimlint.lua
+++ lua/plugins/masonnvimlint.lua
@@ -1,28 +1,28 @@
 return {
-  "rshkarin/mason-nvim-lint",
-  dependencies = {
-    "williamboman/mason.nvim",
-    "mfussenegger/nvim-lint",
-  },
-  config = function()
-    require("mason-nvim-lint").setup({
-      ensure_installed = {
-        "cmakelint",
-        "stylelint",
-        "editorconfig-checker",
-        "golangci-lint",
-        "htmlhint",
-        "deno",
-        "jsonlint",
-        "markdownlint-cli2",
-        "mypy",
-        "pylint",
-        "ruff",
-        "shellcheck",
-        "sqlfluff",
-        "yamllint",
-      },
-      automatic_installation = false,
-    })
-  end,
+    "rshkarin/mason-nvim-lint",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "mfussenegger/nvim-lint",
+    },
+    config = function()
+        require("mason-nvim-lint").setup({
+            ensure_installed = {
+                "cmakelint",
+                "stylelint",
+                "editorconfig-checker",
+                "golangci-lint",
+                "htmlhint",
+                "deno",
+                "jsonlint",
+                "markdownlint-cli2",
+                "mypy",
+                "pylint",
+                "ruff",
+                "shellcheck",
+                "sqlfluff",
+                "yamllint",
+            },
+            automatic_installation = false,
+        })
+    end,
 }
