2efbf34aeabda2b1f0e24308f1ecd74fd2ecd606
h4mst3r <53465855+H4MST3R3N3RGYBALL@users.noreply.github.com>
2024-11-17 02:51:25 -0500

    Add Remote

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "*",
+    callback = function(opts)
+        if vim.bo[opts.buf].omnifunc == "" then
+            vim.bo[opts.buf].omnifunc = "syntaxcomplete#Complete"
+        end
+    end,
+})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,13 +1,5 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.g.python3_host_prog = vim.fn.expand("~/.virtualenvs/neovim/bin/python3")
 vim.g.lazyvim_python_lsp = "basedpyright"
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "*",
-  callback = function(opts)
-    if vim.bo[opts.buf].omnifunc == "" then
-      vim.bo[opts.buf].omnifunc = "syntaxcomplete#Complete"
-    end
-  end,
-})
